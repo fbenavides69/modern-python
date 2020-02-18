78bfe8277ac67dca254dd11b588a5b2f2dfb5fca
Francisco Benavides <francisco.benavides@gmail.com>
2020-02-18 00:08:25 -0500

    Second draft: Setup Article
    Second script: Extract Wikipedia random poetry summary, with user
    locale, try/except response status handling, and click text title
    foreground color setup and summary test fillup.

--- src/modern_python/console.py
+++ src/modern_python/console.py
@@ -1,4 +1,9 @@
+import sys
+import locale
+import textwrap
+
 import click
+import requests
 
 from . import __version__
 
@@ -6,5 +11,24 @@ from . import __version__
 @click.command()
 @click.version_option(version=__version__)
 def main():
-    """The hypermodern Python project."""
-    click.echo("Hello, world!")
+    """The Modern Python project."""
+    try:
+        userLocale = locale.getlocale()[0].split("_")[0]
+        API_URL: str = f"https://{userLocale}.wikipedia.org/api/rest_v1/page/random/summary"
+
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            data = response.json()
+
+            title = data["title"]
+            extract = data["extract"]
+
+            click.secho(title, fg="green")
+            click.echo(textwrap.fill(extract))
+
+    except (requests.exceptions.Timeout,
+            requests.ConnectionError,
+            requests.exceptions.TooManyRedirects,
+            requests.exceptions.RequestException) as e:
+        print(str(e))
+        sys.exit(1)
